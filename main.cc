6040e15e8cc4310273e454f42cf960026a7b37a5
atung3 <atung3@stanford.edu>
2018-04-12 08:41:41 +0000

    Added basic steps to the webrtc (creating peer connection objects)

--- main.cc
+++ main.cc
@@ -2,13 +2,28 @@
 #include <webrtc/api/peerconnectioninterface.h>
 #include <webrtc/base/ssladapter.h>
 #include <webrtc/base/thread.h>
+#include <iostream>
 
+// Refer to the API at: https://webrtc.googlesource.com/src/+/master/api/peerconnectioninterface.h
 
-int main(int argc, char **argv) {
-    rtc::InitializeSSL();
-    rtc::InitRandom(rtc::Time());
-    rtc::ThreadManager::Instance()->WrapCurrentThread();
-    
+void OnDataChannelCreated(webrtc::DataChannelInterface* channel);
+void OnIceCandidate(const webrtc::IceCandidateInterface* candidate);
+void OnDataChannelMessage(const webrtc::DataBuffer& buffer);
+void OnAnswerCreated(webrtc::SessionDescriptionInterface* desc);
+
+// Set up web sockets from boost
+
+// 1. Peer Connection Factory that sets up the signaling and worker threads
+rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> peer_connection_factory;
+// 2. Peer Connection
+rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection;
+// 2. Peer Connection Observer
+PeerConnectionObserver peer_connection_observer(OnDataChannelCreated, OnIceCandidate);
+// 2. Configuration Settings
+webrtc::PeerConnectionInterface::RTCConfiguration configuration;
+
+void createPeerConnectionInterface() {
+
     rtc::Thread *signalingThread = new rtc::Thread();
     rtc::Thread *workerThread = new rtc::Thread();
 
@@ -19,8 +34,34 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // questionable arguments compared to sample
-    rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> pcfactory = 
-        webrtc::CreatePeerConnectionFactory(workerThread, signalingThread,
-                                            NULL, NULL, NULL);
+    peer_connection_factory = webrtc::CreatePeerConnectionFactory(workerThread, signalingThread, NULL, NULL, NULL).release();
+}
+
+void createPeerConnection() {
+	// Set up configuration for STUN server
+	webrtc::PeerConnectionInterface::IceServer ice_server;
+	ice_server.uri = "stun:stun.l.google.com:19302";
+	configuration.servers.push_back(ice_server);
+
+	peer_connection = peer_connection_factory->CreatePeerConnection(configuration, nullptr, nullptr, &peer_connection_observer);
+}
+
+int main(int argc, char **argv) {
+
+	// Initialize ssl and thread manager
+    rtc::InitializeSSL();
+    rtc::InitRandom(rtc::Time());
+    rtc::ThreadManager::Instance()->WrapCurrentThread();
+	
+	
+
+    // 1. Create a PeerConnectionFactoryInterface
+	createPeerConnectionInterface()
+
+	// 2. Create a PeerConnection object with configuration and PeerConnectionObserver
+    createPeerConnection();
+
+	rtc::CleanupSSL();
+
+	return 0;
 }
