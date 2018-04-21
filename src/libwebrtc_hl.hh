c83d54f2efab6312b96c4d67df03244c9296c6dd
John Emmons <emmons.john@gmail.com>
2018-04-21 21:24:07 +0000

    more progress on autoconf setup

---
+++ src/libwebrtc_hl.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <webrtc/api/mediastreaminterface.h>
+#include <webrtc/api/peerconnectioninterface.h>
+#include <webrtc/base/ssladapter.h>
+#include <webrtc/base/thread.h>
+#include <webrtc/base/physicalsocketserver.h>
