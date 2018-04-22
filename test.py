67d7cdbce8aba6d2fc27577365707759f6b66b51
John Emmons <emmons.john@gmail.com>
2018-04-22 07:20:47 +0000

    added note for test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+### 
+#
+# NOTE: you have to do the following things to get this code to work...
+#
+# 1. export PYTHONPATH=python/build/lib.linux-x86_64-3.5
+# 2. export LD_LIBRARY_PATH=src/.libs
+# 
+###
+
 import pywebrtc 
 
 conn = pywebrtc.Connection('hello')
